496c1787e11ec3e93fec3fa8e91d59e7172e6fe7
Brandon Diep <brandondiep2711@gmail.com>
2024-03-07 16:59:10 -0800

    updated the formatting and styling of the header and user's reviews

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -8,6 +8,24 @@ module.exports = {
       const userProfile = await User.findOne({userName: req.params.id});
       const popularReviews = await Review.find({ user: userProfile.id }).sort({ reviewLikes: 'desc' }).populate('user');
       const recentReviews = await Review.find({ user: userProfile.id }).sort({ createdAt: 'desc' }).populate('user');
+      const BASE_URL = 'https://www.themoviedb.org/t/p/w94_and_h141_bestv2'
+      const MOVIEAPI_KEY = process.env.MOVIEAPI_KEY
+
+
+      const movieIds = []
+
+      for(const review of recentReviews){
+        movieIds.push(review.movieId)
+      }
+
+      const movieDetails = []
+
+      for(const movieId of movieIds){
+        const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${MOVIEAPI_KEY}&language=en-US`)
+        const movie = await response.json()
+        movieDetails.push(movie)
+      }
+
 
       if(userProfile){
         res.render("profile.ejs", {
@@ -15,6 +33,8 @@ module.exports = {
           userProf: userProfile,
           recentReviews: recentReviews,
           popularReviews: popularReviews,
+          movieDetails: movieDetails,
+          base_url: BASE_URL,
           userId: req.user.id,
           userStatus: {
             loggedIn: true
