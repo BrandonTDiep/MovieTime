9bca81f619ba5f19785607bdf27667504a28c75c
Brandon Diep <brandondiep2711@gmail.com>
2024-03-17 14:06:40 -0700

    removed the checking of user profile in profiles.js controller since using 'ensureUserExists' middleware

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -9,59 +9,54 @@ module.exports = {
       const MOVIEAPI_KEY = process.env.MOVIEAPI_KEY
       const base_url2 = 'https://www.themoviedb.org/t/p/w220_and_h330_face'
 
-      if(userProfile){
-        const popularReviews = await Review.find({ user: userProfile.id }).sort({ reviewLikes: 'desc' }).populate('user');
-        const recentReviews = await Review.find({ user: userProfile.id }).sort({ createdAt: 'desc' }).populate('user');
+      const popularReviews = await Review.find({ user: userProfile.id }).sort({ reviewLikes: 'desc' }).populate('user');
+      const recentReviews = await Review.find({ user: userProfile.id }).sort({ createdAt: 'desc' }).populate('user');
 
-        const movieIds = []
+      const movieIds = []
 
-        for(const review of recentReviews){
-          movieIds.push(review.movieId)
-        }
+      for(const review of recentReviews){
+        movieIds.push(review.movieId)
+      }
 
-        const movieDetails = []
+      const movieDetails = []
 
-        for(const movieId of movieIds){
-          const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${MOVIEAPI_KEY}&language=en-US`)
-          const movie = await response.json()
-          movieDetails.push(movie)
-        }
+      for(const movieId of movieIds){
+        const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${MOVIEAPI_KEY}&language=en-US`)
+        const movie = await response.json()
+        movieDetails.push(movie)
+      }
 
-        const favFilms = userProfile.favFilms.sort((a, b) => a.position - b.position)
-        if(req.user){
-          res.render("profile.ejs", {
-            user: req.user,
-            userProf: userProfile,
-            recentReviews: recentReviews,
-            popularReviews: popularReviews,
-            movieDetails: movieDetails,
-            favFilms: favFilms,
-            base_url: BASE_URL,
-            base_url2: base_url2,
-            userId: req.user.id,
-            userStatus: {
-              loggedIn: true
-            },
-          });
-        }
-        else{
-          res.render("profile.ejs", {
-            user: req.user,
-            userProf: userProfile,
-            recentReviews: recentReviews,
-            popularReviews: popularReviews,
-            movieDetails: movieDetails,
-            favFilms: favFilms,
-            base_url: BASE_URL,
-            base_url2: base_url2,
-            userStatus: {
-              loggedIn: false
-            },
-          });
-        }
+      const favFilms = userProfile.favFilms.sort((a, b) => a.position - b.position)
+      if(req.user){
+        res.render("profile.ejs", {
+          user: req.user,
+          userProf: userProfile,
+          recentReviews: recentReviews,
+          popularReviews: popularReviews,
+          movieDetails: movieDetails,
+          favFilms: favFilms,
+          base_url: BASE_URL,
+          base_url2: base_url2,
+          userId: req.user.id,
+          userStatus: {
+            loggedIn: true
+          },
+        });
       }
       else{
-        res.render("error.ejs");
+        res.render("profile.ejs", {
+          user: req.user,
+          userProf: userProfile,
+          recentReviews: recentReviews,
+          popularReviews: popularReviews,
+          movieDetails: movieDetails,
+          favFilms: favFilms,
+          base_url: BASE_URL,
+          base_url2: base_url2,
+          userStatus: {
+            loggedIn: false
+          },
+        });
       }
     } catch (err) {
       console.log(err);
